f63f1b5fe38a03b5dcd71a2b4ee2ef6b83b3c1ce
ryabudylin <ryabudylin@avito.ru>
2024-06-06 20:17:55 +0300

    update environment setup in homework week 2

--- homework_solutions/hw_week_2.py
+++ homework_solutions/hw_week_2.py
@@ -1,1 +1,26 @@
-# docker-compose.yaml настраивает все как нужно
+# настроить токены для почты https://stepik.org/lesson/1074008/step/13?unit=1084080
+# сразу можно настроить S3 https://stepik.org/lesson/1074010/step/3?unit=1084082
+# добавить токены в .env по примеру .env_example
+# docker-compose up -d --build
+# в airflow в Admin - Connections руками добавить s3_connection и pg_connection
+# настройки s3_connection
+# {
+#     "connection Id": "s3_connection",
+#     "connection Type": "Amazon Web Services",
+#     "AWS Access Key ID": AWS_ACCESS_KEY_ID,
+#     "AWS Secret Access Key": AWS_SECRET_ACCESS_KEY,
+#     "Extra": {
+#           "endpoint_url": "https://storage.yandexcloud.net",
+#           "region_name": "ru-centrali"
+#     }
+# }
+# настройки pg_connection
+# {
+#     "connection Id": "pg_connection",
+#     "connection Type": "Postgres",
+#     "Host": "postgres",
+#     "Database": "postgres",
+#     "Login": "airflow",
+#     "Password": "airflow",
+#     "Extra": {}
+# }
